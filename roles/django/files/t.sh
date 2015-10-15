b6e65f2f8891b11d7ca91f7ca6d56a387a292b6d
Mikhail Mikhalap <mikhail_mikhalap@epam.com>
2015-10-15 20:56:56 +0900

    modified:   hosts 	modified:   roles/django/files/t.sh 	modified:   roles/django/tasks/main.yml 	modified:   site.yml

--- roles/django/files/t.sh
+++ roles/django/files/t.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-echo $PWD > imhere
+echo $PWD >> log_django
+DATE=`date`
 #https://www.python.org/ftp/python/3.5.0/Python-3.5.0.tgz
 mysql -e "CREATE DATABASE IF NOT EXISTS dj CHARACTER SET utf8"
-#django-admin.py startproject mysite
+if [ ! -d $1 ]
+	then
+	  django-admin.py startproject $1 && echo "$DATE $1 created" >> log_django
+	else
+	  echo "$DATE $1 exists" >> log_django
+fi

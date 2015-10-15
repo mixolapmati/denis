152a29864b3fb5f7ccc591b3d1d02c563574ff35
Mikhail Mikhalap <mikhail_mikhalap@epam.com>
2015-10-15 20:21:09 +0900

    modified:   roles/django/files/t.sh

--- roles/django/files/t.sh
+++ roles/django/files/t.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo $PWD
+echo $PWD > imhere
 #https://www.python.org/ftp/python/3.5.0/Python-3.5.0.tgz
 mysql -e "CREATE DATABASE IF NOT EXISTS dj CHARACTER SET utf8"
+#django-admin.py startproject mysite

7a4269523d44326c1db44d3a7c27665631b9546f
Mikhail Mikhalap <mikhail_mikhalap@epam.com>
2015-10-21 19:29:50 +0900

    modified:   roles/django/files/deploy_apps.sh 	modified:   roles/django/tasks/main.yml 	modified:   site.yml

--- roles/django/files/deploy_apps.sh
+++ roles/django/files/deploy_apps.sh
@@ -1,14 +1,18 @@
 MAIN_APP=$1
 APP=$2
 GIT_URL=$3
+LOG_FILE=$4
 
 cd $MAIN_APP
 
 if [ ! -d $APP ]
        then
-         git clone $GIT_URL -b $APP  && echo "$(date) catalog $APP cloned" >> log_django
+         git clone $GIT_URL -b $APP  && echo "$(date) catalog $APP cloned" >> $LOG_FILE
        else
-         echo "$DATE Catalog $APP exists" >> log_django
+         echo "$DATE Catalog $APP exists" >> $LOG_FILE
+         cd $APP && git pull
+         echo "DATE Command git pull performed" >> $LOG_FILE
 fi
 
 #python manage.py makemigrations {{ apps }}
+#cd $1 && python manage.py migrate

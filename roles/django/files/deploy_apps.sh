ccd427f4b2f430f751954909472dc0490362a8b0
Mikhail Mikhalap <mikhail_mikhalap@epam.com>
2015-10-21 22:12:46 +0900

    modified:   roles/django/files/deploy_apps.sh

--- roles/django/files/deploy_apps.sh
+++ roles/django/files/deploy_apps.sh
@@ -2,18 +2,18 @@ MAIN_APP=$1
 APP=$2
 GIT_URL=$3
 LOG_FILE=$4
-
+WORKDIR="/root/${MAIN_APP}"
 cd ${MAIN_APP}
 
 if [ ! -d ${APP} ]
        then
          echo "$(date) Catalog ${APP} not exists" >> ${LOG_FILE}
-	 ansible-playbook site.yml --tags git_clone --extra-vars app=${APP} git_url=${GIT_URL} && echo "$(date) catalog ${APP} was cloned" >> ${LOG_FILE}
+	 git clone ${GIT_URL} -b ${APP} ${APP} && echo "$(date) catalog ${APP} was cloned" >> ${LOG_FILE}
        else
          echo "$(date) Catalog ${APP} exists" >> ${LOG_FILE}
          cd ${APP} && git pull
          echo "DATE Command git pull was performed" >> ${LOG_FILE}
 fi
 #python manage.py makemigrations {{ apps }}
-#cd $1 && python manage.py migrate
+#cd WORKDIR && python manage.py migrate
 

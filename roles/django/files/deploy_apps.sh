39dcfcc49a15abf7e8f17a9fa405659dc4f2ffaf
Mikhail Mikhalap <mikhail_mikhalap@epam.com>
2015-10-21 19:11:38 +0900

    modified:   roles/django/files/deploy_apps.sh

--- roles/django/files/deploy_apps.sh
+++ roles/django/files/deploy_apps.sh
@@ -6,7 +6,7 @@ cd $MAIN_APP
 
 if [ ! -d $APP ]
        then
-         git clone $GIT_URL -b $APP $APP  && echo "$(date) catalog $APP cloned" >> log_django
+         git clone $GIT_URL -b $APP  && echo "$(date) catalog $APP cloned" >> log_django
        else
          echo "$DATE Catalog $APP exists" >> log_django
 fi
